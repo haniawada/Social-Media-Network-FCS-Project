89444f7337d55ea2c82ca9103871ac47e3d2a033
haniawada <haniawadaa0@gmail.com>
2024-07-25 01:16:45 +0300

    Defining class user and init method

--- Social-Network.py
+++ Social-Network.py
@@ -1,2 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+
+class User:
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+        self.friends = set()
