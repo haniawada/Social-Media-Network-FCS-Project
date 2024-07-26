a4f45aeca0a45a3c04ba5db6c304ee82484ad337
haniawada <haniawadaa0@gmail.com>
2024-07-26 17:53:44 +0300

    Add add friend method to User class

--- Social-Network.py
+++ Social-Network.py
@@ -6,3 +6,6 @@ class User:
         self.user_id = user_id
         self.name = name
         self.friends = set()
+        
+    def add_friend(self, friend_id):
+        self.friends.add(friend_id)
